4573b2a2d87fbd7e50887c3603f1cf19b4a20ccf
FunafutiTV <113341799+FunafutiTV@users.noreply.github.com>
2023-11-18 16:22:59 +0100

    fixed background color issue

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -5,9 +5,9 @@ import { Box } from "@chakra-ui/react"
 
 export default function Home() {
   return (
-  <Box width="100vw" height="100vh" bgGradient="linear(to-br, rgba(255,255,255,1), rgba(255,255,255,1) 20%, rgba(236,227,241,1))">
-     <Header />
-     <CurrentDisplay/>
-  </Box> 
+    <Box minHeight="100vh" bgGradient="linear(to-br, rgba(255,255,255,1), rgba(255,255,255,1) 20%, rgba(236,227,241,1))">
+      <Header />
+      <CurrentDisplay/>
+    </Box>
   )
 }
