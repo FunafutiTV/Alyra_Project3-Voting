85a279773cda75f7d3159e4d20285ea3921bda57
FunafutiTV <113341799+FunafutiTV@users.noreply.github.com>
2023-11-19 20:15:22 +0100

    edited VoteOver component to fix an issue

--- frontend/components/VoteOver/VoteOver.js
+++ frontend/components/VoteOver/VoteOver.js
@@ -5,6 +5,7 @@ import { Flex, Button, useToast, Heading } from '@chakra-ui/react';
 
 // Wagmi
 import { readContract, getWalletClient } from '@wagmi/core';
+import { useAccount, usePublicClient } from 'wagmi';
 
 // ReactJS
 import { useState, useEffect } from 'react'
@@ -25,6 +26,19 @@ const VoteOver = () => {
 
     // Toast
     const toast = useToast();
+
+    // wrkStatus
+    const [wrkStatus, setWrkStatus] = useState(false)      
+    
+    // isOwner
+    const [isOwner, setIsOwner] = useState(false);
+
+    // isVoter
+    const [isVoter, setIsVoter] = useState(false);
+
+        
+    // Account's informations
+    const { address, isConnected } = useAccount();
 
     // Get winning proposal ID
     const getWinner = async() => {
@@ -67,6 +81,58 @@ const VoteOver = () => {
         }
     }
 
+        // Verify is user is the owner of the contract
+        const getIsOwner = async() => {
+            try {
+                const data = await readContract({
+                    address: contractAddress,
+                    abi: abi,
+                    functionName: 'owner',
+                })
+                setIsOwner((data === address))
+            }
+            catch(err) {
+                console.log(err.message)
+                toast({
+                    title: 'Error',
+                    description: "An error occured.",
+                    status: 'error',
+                    duration: 4000,
+                    isClosable: true,
+                })
+            }
+        }
+
+        // Verify is user is a voter
+        const getIsVoter = async () => {
+           const walletClient = await getWalletClient();
+           try {
+               const data = await readContract({
+                   address: contractAddress,
+                   abi: abi,
+                   functionName: "getVoter",
+                   args: [address],
+                   account: walletClient.account,
+               });
+           
+               setIsVoter(data.isRegistered);
+           } 
+           catch (err) {
+               console.log(err);
+               setIsVoter(false);
+           }
+        }
+    
+    // Verify if user is the owner of the contract or a voter whenever the user address changes
+    useEffect(() => {
+        const call = async() => {await getIsOwner()};
+        call();
+    }, [address])
+    useEffect(() => {
+        const call = async() => {await getIsVoter()};
+        call();
+    }, [address])
+
     // Get proposals description when winning proposal ID is retrieved
     useEffect(() => {
         const call = async() => {await getDescription()}
@@ -79,10 +145,12 @@ const VoteOver = () => {
                     <Button colorScheme='green' onClick={getWinner}>Get winner</Button>
                 </Flex>
                 <Flex>
-                    {hasClicked ? 
-                        winnerDescription ?
-                            <Heading as="h2" fontSize="1.2rem" marginTop="2rem">The winner is proposal {winner} : {winnerDescription}</Heading>
-                        : <Heading as="h2" fontSize="1.2rem" marginTop="2rem">The winner is proposal number {winner}</Heading> 
+                    {hasClicked ?  isVoter ?
+                        // 
+                        <Heading as="h2" fontSize="1.2rem" marginTop="2rem">The winner is proposal {winner} : {winnerDescription}</Heading>   
+                        : isOwner && winnerDescription ?
+                            <Heading as="h2" fontSize="1.2rem" marginTop="2rem">The winner is proposal number {winner}</Heading> 
+                            : <></>
                     : <></>
                     }
                 </Flex>
